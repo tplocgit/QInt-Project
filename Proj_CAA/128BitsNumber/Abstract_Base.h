b38a7b84123a46be751ed889eb00d0e1d8fef25c
Lộc Trần Phước <giongnhuntlf@gmail.com>
2019-11-06 22:39:18 +0700

    Update Template Class

---
+++ Proj_CAA/128BitsNumber/Abstract_Base.h
@@ -0,0 +1,39 @@
+#pragma once
+#include"pch.h"
+
+class Abstract_Base{
+public:// Set max min value as string here
+	
+private:
+	int64_t* Bits;
+public:// Calculating operator
+	Abstract_Base* operator+(const Abstract_Base& num);
+	Abstract_Base* operator-(const Abstract_Base& num);
+	Abstract_Base* operator*(const Abstract_Base& num);
+	Abstract_Base* operator/(const Abstract_Base& num);
+public:// Bitwise operator
+	Abstract_Base* operator&(const Abstract_Base& num)const;
+	Abstract_Base* operator|(const Abstract_Base& num)const;
+	Abstract_Base* operator^(const Abstract_Base& num)const;
+	Abstract_Base* operator~();
+public:// Shift operator
+	Abstract_Base* operator<<(int bits);
+	Abstract_Base* operator>>(int bits);
+public:// Ro operator
+	Abstract_Base* ROL();
+	Abstract_Base* ROR();
+public:// Convert method
+	virtual Abstract_Base* toDec() = 0;
+	virtual Abstract_Base* toBin() = 0;
+	virtual Abstract_Base* toHex() = 0;
+public:// Some support method
+	void setBit(char index, bool value);
+	Abstract_Base* Negative(const Abstract_Base& num) ;
+	string BitsToString();
+	virtual string toString() = 0;
+public:
+	Abstract_Base();
+	Abstract_Base(const Abstract_Base& num);
+	virtual ~Abstract_Base();
+};
+
--- Proj_CAA/128BitsNumber/Base_10.h
+++ Proj_CAA/128BitsNumber/Base_10.h
@@ -1,8 +1,6 @@
 #pragma once
-#include "NumBase.h"
-class Base_10 :
-	public NumBase
-{
+#include "Abstract_Base.h"
+class Base_10 : public Abstract_Base {
 public:
 	Base_10();
 	~Base_10();
